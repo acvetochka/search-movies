d2f89b65ab08e5c3922862e51affafe993b51afa
Alona Kuznietsova <acvetochka@gmail.com>
2023-06-23 18:11:50 +0200

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import Home from 'pages/Home';
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="/movies:movieId" element={<div>MovieDetail</div>} />
+        <Route path="*" element={<div>NotFound</div>} />
+      </Routes>
+
+      {/* '/' – компонент Home, домашня сторінка зі списком популярних кінофільмів.
+      '/movies' – компонент Movies, сторінка пошуку кінофільмів за ключовим
+      словом. '/movies/:movieId' – компонент MovieDetails, сторінка з детальною
+      інформацією про кінофільм. /movies/:movieId/cast – компонент Cast,
+      інформація про акторський склад. Рендериться на сторінці MovieDetails.
+      /movies/:movieId/reviews – компонент Reviews, інформація про огляди.
+      Рендериться на сторінці MovieDetails. */}
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import { useEffect } from 'react';
+
+function Home() {
+  useEffect(() => {}, []);
+  return <div>Home</div>;
+}
+
+export default Home;
