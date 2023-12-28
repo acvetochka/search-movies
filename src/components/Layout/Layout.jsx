39751a9d1bd069005cf1dd516725ff36877bc715
Alona Kuznietsova <acvetochka@gmail.com>
2023-12-28 23:35:02 +0100

    update readme

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navi, Header, Main, StyledLink } from './Layout.styled';
+// import logo from '../../images/film.png';
 
 function Layout() {
   return (
     <div>
       <Header>
+        {/* <img src={logo} alt="movie logo" width={60} /> */}
         <Navi>
           <StyledLink to="/">Home</StyledLink>
           <StyledLink to="/movies">Movies</StyledLink>
